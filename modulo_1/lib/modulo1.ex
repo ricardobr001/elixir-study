c1ade4f7d2c679caee0900f63ca66a11093de020
Ricardo Mendes <ricardomlealjr@gmail.com>
2022-01-09 21:19:04 -0300

    initial commit

---
+++ modulo_1/lib/modulo1.ex
@@ -0,0 +1,5 @@
+defmodule Modulo1 do
+  def hello do
+    :world
+  end
+end
---
+++ modulo_1/test/modulo1_test.exs
@@ -0,0 +1,8 @@
+defmodule Modulo1Test do
+  use ExUnit.Case
+  doctest Modulo1
+
+  test "greets the world" do
+    assert Modulo1.hello() == :world
+  end
+end
